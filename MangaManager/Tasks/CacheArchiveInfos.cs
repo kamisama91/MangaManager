224c4416c061e4ef2478789910c789873584e74d
Alexandre Piel <alexandre.1984@free.fr>
2024-04-21 14:51:22 +0200

    Fix concurrent access to CacheArchiveInfos counters

--- MangaManager/Tasks/CacheArchiveInfos.cs
+++ MangaManager/Tasks/CacheArchiveInfos.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Threading;
 using MangaManager.Models;
 
 namespace MangaManager.Tasks
 {
     public static class CacheArchiveInfos
     {
-        private static ConcurrentDictionary<string, ArchiveInfo> s_ArchiveInfoCache = new ConcurrentDictionary<string, ArchiveInfo>();
+        public struct CacheArchiveInfosCounters
+        {
+            public int Hits;
+            public int Misses;
+        }
 
-        public static int Hits { get; private set; }
-        public static int Misses { get; private set; }
+        private static ConcurrentDictionary<string, ArchiveInfo> s_ArchiveInfoCache = new ConcurrentDictionary<string, ArchiveInfo>();
+        private static CacheArchiveInfosCounters s_Counters = new CacheArchiveInfosCounters();
+
+        public static int Hits => s_Counters.Hits;
+        public static int Misses => s_Counters.Misses;
 
         public static bool Exists(string path)
         {
             var result = s_ArchiveInfoCache.ContainsKey(path);
-            if (result) { Hits++; }
-            else { Misses++; }
+            if (result) { Interlocked.Increment(ref s_Counters.Hits); }
+            else { Interlocked.Increment(ref s_Counters.Misses); }
             return result;
         }
 
@@ -45,7 +53,7 @@ namespace MangaManager.Tasks
         public static void UpdatePath(string oldPath, string newPath)
         {
             if (!Exists(oldPath)) { return; }
-            if (Exists(newPath)) { throw new InvalidOperationException("ArchiveInfo already cached"); } else { Misses--; /*Do Not count this miss has it is expected one*/ }
+            if (Exists(newPath)) { throw new InvalidOperationException("ArchiveInfo already cached"); } else { Interlocked.Decrement(ref s_Counters.Misses); ; /*Do Not count this miss has it is expected one*/ }
 
             var info = Get(oldPath);
             CreateOrUpdate(newPath, info);
