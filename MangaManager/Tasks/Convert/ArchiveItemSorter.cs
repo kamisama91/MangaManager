7ed9accf3e9d984fc6466fdf04a3a8feeb8c9a62
Alexandre Piel <alexandre.1984@free.fr>
2024-07-23 16:34:09 +0200

    Fix an issue in archive file ordering when item names are not consistent

--- MangaManager/Tasks/Convert/ArchiveItemSorter.cs
+++ MangaManager/Tasks/Convert/ArchiveItemSorter.cs
@@ -8,18 +8,40 @@ namespace MangaManager.Tasks.Convert
 {
     public static class ArchiveItemSorter
     {
+        private static Regex s_IsCoverRegex = new Regex(@"^(cover|0+\w?)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         private static Regex s_ExtractLastDigitsGroupRegex = new Regex(@"^.*?(\d+)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         public static IEnumerable<T> OrderForArchiving<T>(this IEnumerable<T> enumerable, Func<T, string> getPath)
         {
             return enumerable
-                .OrderBy(t => int.TryParse(Path.GetDirectoryName(getPath(t)), out var folderNum) ? folderNum : int.MaxValue)                                                        //Folder is a number
-                .ThenBy(t => int.TryParse(s_ExtractLastDigitsGroupRegex.Replace(Path.GetDirectoryName(getPath(t)), "$1"), out var folderNum) ? folderNum : int.MaxValue)            //Then Folder ends with a number
-                .ThenBy(t => Path.GetDirectoryName(getPath(t)).ToLowerInvariant())                                                                                                  //Then Folder alphabetical order
-                .ThenBy(t => Path.GetFileNameWithoutExtension(getPath(t)).Equals("cover", StringComparison.InvariantCultureIgnoreCase) ? 0 : int.MaxValue)                          //Then "cover" file always first in its Folder
-                .ThenBy(t => int.TryParse(Path.GetFileNameWithoutExtension(getPath(t)), out var fileNum) ? fileNum : int.MaxValue)                                                  //Then File is a number in its Folder
-                .ThenBy(t => int.TryParse(s_ExtractLastDigitsGroupRegex.Replace(Path.GetFileNameWithoutExtension(getPath(t)), "$1"), out var fileNum) ? fileNum : int.MaxValue)     //Then File ends with a a number in its Folder
-                .ThenBy(t => Path.GetFileNameWithoutExtension(getPath(t)).ToLowerInvariant());                                                                                      //Then File alphabetical order in its Folder
+                .Select(t => new
+                {
+                    Item = t,
+                    FullPath = getPath(t),
+                    
+                    DirectoryName = Path.GetDirectoryName(getPath(t)).ToLowerInvariant(),
+                    DirectoryOrder =
+                            int.TryParse(Path.GetDirectoryName(getPath(t)), out var folderNum)                                                                      //Folder is a number
+                            ? folderNum
+                            : int.TryParse(s_ExtractLastDigitsGroupRegex.Replace(Path.GetDirectoryName(getPath(t)), "$1"), out var regexpFolderNum)                 //Then Folder ends with a number
+                                ? regexpFolderNum
+                                : int.MaxValue,                                                                                                                     //Then Folder alphabetical order
+
+                    FileNameWithoutExtension = Path.GetFileNameWithoutExtension(getPath(t)).ToLowerInvariant(),                                                 
+                    FileNameWithoutExtensionOrder =
+                            s_IsCoverRegex.IsMatch(Path.GetFileNameWithoutExtension(getPath(t)))                                                                    //Cover files always first in its Folder
+                            ? 0
+                            : int.TryParse(Path.GetFileNameWithoutExtension(getPath(t)), out var fileNum)                                                           //Then File is a number
+                                ? fileNum
+                                : int.TryParse(s_ExtractLastDigitsGroupRegex.Replace(Path.GetFileNameWithoutExtension(getPath(t)), "$1"), out var regexFileNum)     //Then File ends with a number
+                                    ? regexFileNum
+                                    : int.MaxValue,                                                                                                                 //Then File alphabetical order
+                })
+                .OrderBy(t => t.DirectoryOrder)
+                .ThenBy(t => t.DirectoryName)
+                .ThenBy(t => t.FileNameWithoutExtensionOrder)
+                .ThenBy(t => t.FileNameWithoutExtension)
+                .Select(t => t.Item);
         }
     }
 }
